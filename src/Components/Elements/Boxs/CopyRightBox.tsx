f28e6c2f9d491ffdccc09381e0debf3a2dc74d98
psmever <psmever@gmail.com>
2021-07-21 11:01:03 +0900

    [DELETE] material ui.

--- src/Components/Elements/Boxs/CopyRightBox.tsx
+++ src/Components/Elements/Boxs/CopyRightBox.tsx
@@ -1,19 +1,3 @@
-import React from 'react';
-import Link from '@material-ui/core/Link';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
-
 export default function CopyRightBox() {
-    return (
-        <Box mt={8}>
-            <Typography variant="body2" color="textSecondary" align="center">
-                {'Copyright © '}
-                <Link color="inherit" href="https://material-ui.com/">
-                    lunatalk
-                </Link>
-                {new Date().getFullYear()}
-                {'.'}
-            </Typography>
-        </Box>
-    );
+    return <>CopyRightBox</>;
 }
--- src/Components/Elements/Spinners/PageSpinner.tsx
+++ src/Components/Elements/Spinners/PageSpinner.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import PulseLoader from 'react-spinners/PulseLoader';
-import { Box } from '@material-ui/core';
 
 export default function PageSpinner() {
     return (
         <React.Fragment>
-            <Box display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
-                <PulseLoader color="#ddd" size="15px" />
-            </Box>
+            <PulseLoader color="#ddd" size="15px" />
         </React.Fragment>
     );
 }
