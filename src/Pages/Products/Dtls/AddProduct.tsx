fbd9763e16d8ca89e184f6bccb13013675574a6d
psmever <psmever@gmail.com>
2021-08-05 01:27:29 +0900

    [Working] products-menu.

--- src/Pages/Products/Dtls/AddProduct.tsx
+++ src/Pages/Products/Dtls/AddProduct.tsx
@@ -12,17 +12,27 @@ export default function AddProduct() {
         // call save API
     };
 
-    const [fileList, setFileList] = useState<any>([
+    const [repfileList] = useState<any>([
         {
             uid: '-1',
             name: 'image.png',
             status: 'done',
-            url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+            url: `${process.env.REACT_APP_MEDIA_URL}/storage/products/rep/8e3121f1cb97b450e3b7623f631dd966811cbfa2/f97ade69-a617-49a3-962e-50dffc9e006a.jpeg`,
         },
     ]);
 
-    const onChange = ({ fileList: newFileList }) => {
-        setFileList(newFileList);
+    const [detailFileList] = useState<any>([
+        {
+            uid: '-1',
+            name: 'image.png',
+            status: 'done',
+            url: `${process.env.REACT_APP_MEDIA_URL}/storage/products/detail/8e8f0ba84b11269265ef00d1744c3b1e950c6840/0033a6e2-5e5b-4fae-ad84-faa992f6cbaf.jpg`,
+        },
+    ]);
+
+    const onChange = ({ fileList }: { fileList: any }) => {
+        // setFileList(newFileList);
+        console.debug(fileList);
     };
 
     const onPreview = async (file: any) => {
@@ -37,7 +47,9 @@ export default function AddProduct() {
         const image = new Image();
         image.src = src;
         const imgWindow = window.open(src);
-        // imgWindow.document.write(image.outerHTML);
+        if (imgWindow) {
+            imgWindow.document.write(image.outerHTML);
+        }
     };
 
     return (
@@ -116,27 +128,42 @@ export default function AddProduct() {
                             label="판매 상태"
                             name="productSaleActive"
                             valuePropName="checked"
-                            initialValue={false}
+                            initialValue={true}
                         >
                             <Switch checkedChildren={<CheckOutlined />} unCheckedChildren={<CloseOutlined />} />
                         </Form.Item>
-                        <Form.Item label="상품 상태" name="productActive" valuePropName="checked" initialValue={false}>
+                        <Form.Item label="상품 상태" name="productActive" valuePropName="checked" initialValue={true}>
                             <Switch checkedChildren={<CheckOutlined />} unCheckedChildren={<CloseOutlined />} />
                         </Form.Item>
                         <Form.Item label="메모" name="productMemo">
                             <Input.TextArea rows={4} />
                         </Form.Item>
-                        <ImgCrop rotate>
-                            <Upload
-                                action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-                                listType="picture-card"
-                                fileList={fileList}
-                                onChange={onChange}
-                                onPreview={onPreview}
-                            >
-                                {fileList.length < 5 && '+ Upload'}
-                            </Upload>
-                        </ImgCrop>
+                        <Form.Item label="대표 사진">
+                            <ImgCrop rotate>
+                                <Upload
+                                    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                                    listType="picture-card"
+                                    fileList={repfileList}
+                                    onChange={onChange}
+                                    onPreview={onPreview}
+                                >
+                                    {repfileList.length < 5 && '+ Upload'}
+                                </Upload>
+                            </ImgCrop>
+                        </Form.Item>
+                        <Form.Item label="상세 사진">
+                            <ImgCrop rotate>
+                                <Upload
+                                    action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                                    listType="picture-card"
+                                    fileList={detailFileList}
+                                    onChange={onChange}
+                                    onPreview={onPreview}
+                                >
+                                    {detailFileList.length < 5 && '+ Upload'}
+                                </Upload>
+                            </ImgCrop>
+                        </Form.Item>
                         <Divider />
                         <Row justify="center">
                             <Button type="primary" htmlType="submit">
